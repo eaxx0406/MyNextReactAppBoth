85abfd22f8d8ff70c51d3b8827e42b631102ef4e
Ea Overvad <ea@0vervad.dk>
2025-09-04 17:30:54 +0200

    Ændering?

--- my-next-app/app/hejtest/page.js
+++ my-next-app/app/hejtest/page.js
@@ -4,6 +4,7 @@
             <h1>Hej Team 09</h1>
             <p>Dette er en simpel Next.js-side!</p>
             <p>Velkommen til vores projekt.</p>
+            <p>ændering.</p>
         </div>
     );
 }
